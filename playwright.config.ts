67eed35a5769e3773e083363eb96ee53882745e4
Andrii Les <andrewlesat@gmail.com>
2025-07-12 14:56:15 +0300

    Config change and browser install skip

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   },
 
   expect: {
-    timeout: 15_000
+    timeout: 5_000
   },
 
   /* Configure projects for major browsers */
@@ -44,15 +44,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
