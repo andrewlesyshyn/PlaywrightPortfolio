42b62aaedb3887ff614d45308e570127b94adc12
Andrii Les <andrewlesat@gmail.com>
2025-07-12 14:16:21 +0300

    Text input v1

--- tests/UItesting/text_inputs.spec.ts
+++ tests/UItesting/text_inputs.spec.ts
@@ -1,13 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
+
+let textinput: Locator;
+let updatingBtn: Locator;
+
+let newName: string = 'NewButtonName';
+
+test.beforeEach(async({page}) => {
+    await page.goto('/textinput');
+    textinput = page.getByTestId('newButtonName');
+    updatingBtn = page.getByTestId('updatingButton');
+})
+
+test('Text input the easy way', async ({ page }) => {
+    await textinput.fill(newName);
+    await expect(textinput).toHaveValue(newName);
+
+    let buttonText = await updatingBtn.textContent();
+    await updatingBtn.click();
+    if(buttonText !== null) await expect(updatingBtn).not.toHaveText(buttonText);
+    await expect(updatingBtn).toHaveText(newName);
+})
 
 test('Text input manually', async ({ page }) => {
-    await page.goto('/textinput');
-    let textinput = page.getByTestId('newButtonName');
     // await textinput.fill('NewButtonName');
     await textinput.click();
     await page.keyboard.type('NewButSlow', { delay: 100 });
 
-    let updatingBtn = page.getByTestId('updatingButton');
+    
     await updatingBtn.click();
     await expect(updatingBtn).toHaveText('NewButSlow')
 })
