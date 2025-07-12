39df2a98f3b2f17283c7e9c946eda22b3d4d55c9
Andrii Les <andrewlesat@gmail.com>
2025-07-12 17:22:36 +0300

    Scroll cases

--- tests/UItesting/scrollbars.spec.ts
+++ tests/UItesting/scrollbars.spec.ts
@@ -1,16 +1,49 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
-test('Text input manually', async ({ page }) => {
+let overflowedContainer: Locator;
+let hidingBtn: Locator;
+
+test.beforeEach(async ({ page }) => {
     await page.goto('/scrollbars');
-    let overflowedContainer = page.locator("//h4[text()='Playground']/following-sibling::div");
-    let hidingBtn = page.getByTestId('hidingButton');
-
+    overflowedContainer = page.locator("//h4[text()='Playground']/following-sibling::div");
+    hidingBtn = page.getByTestId('hidingButton');
     // await expect(hidingBtn).toBeHidden(); // In this case, element has property 'hidden: false' so it is visible, but overflown by another element
     await expect(hidingBtn).not.toBeInViewport(); // So it could be useful to use viewport assertion
-    // await hidingBtn.scrollIntoViewIfNeeded();
+})
+
+test('Just click, even if not in viewport', async () => {
     await hidingBtn.click();
     await expect(hidingBtn).toHaveCSS('box-shadow', 'rgba(0, 123, 255, 0.5) 0px 0px 0px 3.2px')
+})
 
-    // box-shadow:
-    //rgba(0, 123, 255, .5);
+test('Hover and mouse down still working', async ({ page }) => {
+    await hidingBtn.hover();
+    await expect(hidingBtn).toBeInViewport();
+    await page.mouse.down();
+    await page.mouse.up();
+    await expect(hidingBtn).toHaveCSS('box-shadow', 'rgba(0, 123, 255, 0.5) 0px 0px 0px 3.2px')
+})
+
+test('Try to move mouse over manually', async ({ page }) => {
+    await hidingBtn.scrollIntoViewIfNeeded();
+    await expect(hidingBtn).toBeInViewport();
+    let box = await hidingBtn.boundingBox();
+    if (box === null) throw new Error("Hiding button has no bounding box");
+    await page.mouse.click(box.x + box.width / 2, box.y + box.height / 2);
+    await expect(hidingBtn).toHaveCSS('box-shadow', 'rgba(0, 123, 255, 0.5) 0px 0px 0px 3.2px')
+})
+
+test('Scroll into manually', async ({ page }) => {
+    await overflowedContainer.click();
+    let containerBox = await overflowedContainer.boundingBox();
+    if (containerBox === null) throw new Error("Container has no bounding box");
+    
+    let box = await hidingBtn.boundingBox();
+    if (box === null) throw new Error("Hiding button has no bounding box");
+
+    await page.mouse.wheel(box.x - containerBox.x, box.y - containerBox.y)
+    await expect(hidingBtn).toBeInViewport();
+
+    await page.mouse.click(containerBox.x, containerBox.y + box.height / 2);
+    await expect(hidingBtn).toHaveCSS('box-shadow', 'rgba(0, 123, 255, 0.5) 0px 0px 0px 3.2px')
 })
