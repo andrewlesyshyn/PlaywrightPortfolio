dff52002be15693fbee1838741dbdd8c0a4c5eb9
Andrii Les <andrewlesat@gmail.com>
2025-07-14 15:27:58 +0300

    Prettier formatting

--- tests/UItesting/click.spec.ts
+++ tests/UItesting/click.spec.ts
@@ -1,45 +1,43 @@
-import { test, expect, Locator } from '@playwright/test';
+import { test, expect, Locator } from "@playwright/test";
 
 let badBtn: Locator;
 
 test.beforeEach(async ({ page }) => {
-    await page.goto("/click");
+  await page.goto("/click");
 
-    badBtn = page.getByTestId('badButton');
-    await expect(badBtn).toHaveCSS('background-color', 'rgb(0, 123, 255)');
-})
+  badBtn = page.getByTestId("badButton");
+  await expect(badBtn).toHaveCSS("background-color", "rgb(0, 123, 255)");
+});
 
-test('Try to emulate', async () => {
-    await badBtn.dispatchEvent('click');
+test("Try to emulate", async () => {
+  await badBtn.dispatchEvent("click");
 
-    await expect(badBtn).not.toHaveCSS('background-color', 'rgb(33, 136, 56)')
-})
+  await expect(badBtn).not.toHaveCSS("background-color", "rgb(33, 136, 56)");
+});
 
-test('Click for real (thanks CDP)', async () => {
-    await badBtn.click();
-    // const finalColor = await badBtn.evaluate((el) => {
-    //     return getComputedStyle(el).backgroundColor;
-    // });
-    // console.log(finalColor);
-    await expect(badBtn).toHaveCSS('background-color', 'rgb(33, 136, 56)')
-})
+test("Click for real (thanks CDP)", async () => {
+  await badBtn.click();
+  // const finalColor = await badBtn.evaluate((el) => {
+  //     return getComputedStyle(el).backgroundColor;
+  // });
+  // console.log(finalColor);
+  await expect(badBtn).toHaveCSS("background-color", "rgb(33, 136, 56)");
+});
 
-test('Click semi-manually', async ({ page }) => {
-    await badBtn.hover();
-    await expect(badBtn).toHaveCSS('background-color', 'rgb(0, 105, 217)')
-    await page.click('#badButton', { position: { x: 10, y: 10 }, clickCount: 5 }); // don't ask
-    await expect(badBtn).toHaveCSS('background-color', 'rgb(33, 136, 56)')
-})
+test("Click semi-manually", async ({ page }) => {
+  await badBtn.hover();
+  await expect(badBtn).toHaveCSS("background-color", "rgb(0, 105, 217)");
+  await page.click("#badButton", { position: { x: 10, y: 10 }, clickCount: 5 }); // don't ask
+  await expect(badBtn).toHaveCSS("background-color", "rgb(33, 136, 56)");
+});
 
-test('Click manually', async ({ page }) => {
-    const box = await badBtn.boundingBox();
-    if (box === null) throw new Error("Bad button has no bounding box")
-    await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
-    await page.mouse.down();
-    await page.mouse.up();
-    // Or just use this code
-    // await page.mouse.click(box.x + box.width / 2, box.y + box.height / 2);
-    await expect(badBtn).toHaveCSS('background-color', 'rgb(33, 136, 56)')
-
-})
-
+test("Click manually", async ({ page }) => {
+  const box = await badBtn.boundingBox();
+  if (box === null) throw new Error("Bad button has no bounding box");
+  await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
+  await page.mouse.down();
+  await page.mouse.up();
+  // Or just use this code
+  // await page.mouse.click(box.x + box.width / 2, box.y + box.height / 2);
+  await expect(badBtn).toHaveCSS("background-color", "rgb(33, 136, 56)");
+});
--- tests/UItesting/dynamic_table.spec.ts
+++ tests/UItesting/dynamic_table.spec.ts
@@ -1,18 +1,24 @@
-import { test, expect, Locator } from '@playwright/test';
+import { test, expect, Locator } from "@playwright/test";
 
-test('Try to handle the table', async({page}) => {
-    await page.goto('/dynamictable');
-    const processName: string = 'Chrome';
-    const resourceName: string = 'CPU';
+test("Try to handle the table", async ({ page }) => {
+  await page.goto("/dynamictable");
+  const processName: string = "Chrome";
+  const resourceName: string = "CPU";
 
-    const table = page.getByRole('table');
-    const columnIndex: number = (await table.getByRole('columnheader').allInnerTexts()).indexOf(resourceName);
+  const table = page.getByRole("table");
+  const columnIndex: number = (
+    await table.getByRole("columnheader").allInnerTexts()
+  ).indexOf(resourceName);
 
-    const row: Locator = table.getByRole('row', {name: processName});
-    const cell: Locator = row.getByRole('cell').nth(columnIndex);
-    
-    const utilization: string | null = await cell.textContent();
-    if(utilization === null) throw new Error(`${processName} ${resourceName} utilization is not found`)
-    await expect(page.getByRole('paragraph').filter({hasText: `${processName} ${resourceName}`})).toContainText(utilization);
+  const row: Locator = table.getByRole("row", { name: processName });
+  const cell: Locator = row.getByRole("cell").nth(columnIndex);
 
-})
+  const utilization: string | null = await cell.textContent();
+  if (utilization === null)
+    throw new Error(`${processName} ${resourceName} utilization is not found`);
+  await expect(
+    page
+      .getByRole("paragraph")
+      .filter({ hasText: `${processName} ${resourceName}` })
+  ).toContainText(utilization);
+});
