1a4965c46332cccf4a53f09705208ca1cc2e12e2
Andrii Les <andrewlesat@gmail.com>
2025-07-20 15:03:02 +0300

    Class attributes test draft

--- tests/UItesting/class_attribute.spec.ts
+++ tests/UItesting/class_attribute.spec.ts
@@ -1,13 +1,39 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Locator } from "@playwright/test";
+
+const attributeName = "box-shadow";
+const attributeValue = "rgba(0, 123, 255, 0.5) 0px 0px 0px 3.2px"
 
 test("Click the primary button", async({page})=>{
     await page.goto("/classattr");
-     
-    const buttons = await page.getByRole("button").all()
+    
+    let primaryBtn!: Locator;
+    const buttons = await page.getByRole("button").all();
+
     for(const element of buttons){
-        let classValue = await element.getAttribute('class');
-        console.log(classValue);
+        const classValue = await element.getAttribute('class');
+        if(classValue?.includes("btn-primary")){
+            primaryBtn = element;
+        }
     }
 
-    // await expect(buttons[0]).tobeVisible();
+    await primaryBtn.click();
+    await expect(primaryBtn).toHaveCSS(attributeName, attributeValue);
+})
+
+test("Get by CSS", async({page})=>{
+    await page.goto("/classattr");
+    
+    const primaryBtn = page.locator(".btn-primary")
+
+    await primaryBtn.click();
+    await expect(primaryBtn).toHaveCSS(attributeName, attributeValue);
+})
+
+test("Get by xPath", async({page})=>{
+    await page.goto("/classattr");
+    
+    const primaryBtn = page.locator("//button[contains(@class, 'btn-primary')]")
+
+    await primaryBtn.click();
+    await expect(primaryBtn).toHaveCSS(attributeName, attributeValue);
 })
